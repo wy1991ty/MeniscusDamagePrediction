a65ea5955bb5e0c8fd620a627af98e98003182b9
wy1991ty <140468495+wy1991ty@users.noreply.github.com>
2024-01-04 09:22:07 +0800

    Add files via upload

--- trian.py
+++ trian.py
@@ -25,11 +25,11 @@ from sklearn import preprocessing
 #import numpy as np
 from metric_learn import LMNN
 # sklearn.datasets import load_iris
-#######数据读取#######
+#######Read data#######
 def read_data(tables,label): 
     x_train, x_test, y_train, y_test =train_test_split(tables, label, test_size=0.3, random_state=0)
     return x_train,y_train,x_test,y_test
-#######Excel转数组###########
+#########Excel to array conversion#########
 def import_excel(excel):
     for rown in range(excel.nrows):
         for col in range(excel.ncols):
@@ -39,40 +39,55 @@ def import_excel(excel):
                 tables[rown,(col-1)]=excel.cell_value(rown,col)
     #print(tables)
     return tables,label
-###数据预处理#######
+###Data preprocessing#######
 def data_preprocessing(values):
-    #数据标准化
+    #data standardization
     data = preprocessing.scale(values,axis=1) 
     return data
- #缺失处理
- #特征优化
 def Select(tables,label):
-      #移除低方差
-      #tables =VarianceThreshold(threshold=(.75 * (1 - .75))).fit_transform(tables)
-      #卡方检测,ANOVA F值，信息熵
+    
      selector=SelectKBest(chi2,k=6)
-      #selector=SelectKBest(f_classif,k=20)#ANOVA F值
-      #selector=SelectKBest(mutual_info_classif,k=10)#信息熵
+
      selector.fit(tables,label)
      print("selected index:",selector.get_support(True))
-      #score=selector.scores_
-      #P=selector.pvalues_
-      #tables=selector.transform(tables)
-      #PCAn_components=10,
-      #pca = PCA(svd_solver='full',whiten=True,copy=True)
-     # tables=pca.fit_transform(tables)
+
      return tables,selector.get_support(True)
-  #度量学习
+ #LNMM
 def metric_learn(tables,label):
-   # iris_data = load_iris()
+
     lmnn = LMNN(k=2, learn_rate=1e-4)
     lmnn.fit(tables,label)
     X_1 = lmnn.transform(tables)
     return  X_1
    
-######逻辑回归#######
-def log_pre(x_train,y_train,x_test,y_test):
-    clf=LogisticRegression(solver='liblinear',max_iter=40)
+########MLP#######
+def MLP_pre(x_train,y_train,x_test,y_test):
+    clf=MLPClassifier(hidden_layer_sizes=(50,50),activation='identity', solver='sgd',
+                      learning_rate='adaptive',learning_rate_init=0.001,alpha=5,
+                      max_iter=5000)
+    clf.fit(x_train,y_train)
+    accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test=classfision(clf,x_train,y_train,x_test,y_test)
+    predict_proba=clf.predict_proba(x_test)
+
+    fpr, tpr, thresholds = roc_curve(y_test, predict_proba[:, 1])
+    roc_auc=auc(fpr,tpr)
+    print("MLP AUC:",roc_auc)
+    plt.figure()
+    plt.title('ROC of MLP in test data_feature select')
+    plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0,1],[0,1],'r--')
+    plt.xlim([0.0,1.0])
+    plt.ylim([0.0,1.0])
+    plt.ylabel('tpr')
+    plt.xlabel('fpr')
+    plt.show()
+    print(predict_proba)
+
+    return accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test,predict_proba
+######RRF######
+def RFF_pre(x_train,y_train,x_test,y_test):
+    clf=RandomForestClassifier(n_estimators=3,max_features=0.3)
     clf.fit(x_train,y_train)
     accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test=classfision(clf,x_train,y_train,x_test,y_test)
     predict_proba=clf.predict_proba(x_test)
@@ -80,9 +95,32 @@ def log_pre(x_train,y_train,x_test,y_test):
     #predict_proba = clf.decision_function(y_test)
     fpr, tpr, thresholds = roc_curve(y_test, predict_proba[:, 1])
     roc_auc=auc(fpr,tpr)
-    print("LOG AUC:",roc_auc)
+    print("RFF AUC:",roc_auc)
     plt.figure()
-    plt.title('ROC of LR in test data_feature select')
+    plt.title('ROC of RF in test data_feature select')
+    plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.legend(loc='lower right')
+    plt.plot([0,1],[0,1],'r--')
+    plt.xlim([0.0,1.0])
+    plt.ylim([0.0,1.0])
+    plt.ylabel('tpr')
+    plt.xlabel('fpr')
+    plt.show()
+    return accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test, predict_proba
+#######Adaboost######
+def boost_pre(x_train,y_train,x_test,y_test):
+    clf=AdaBoostClassifier(learning_rate=1.11)
+    clf.fit(x_train,y_train)
+    accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test=classfision(clf,x_train,y_train,x_test,y_test)
+    predict_proba=clf.predict_proba(x_test)
+    print(predict_proba)
+
+    fpr, tpr, thresholds = roc_curve(y_test, predict_proba[:, 1])
+    roc_auc=auc(fpr,tpr)
+    print("BOOST AUC:",roc_auc)
+    plt.figure()
+    plt.title('ROC of BOOST in test data_feature select')
     plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
     plt.legend(loc='lower right')
     plt.plot([0,1],[0,1],'r--')
@@ -92,19 +130,19 @@ def log_pre(x_train,y_train,x_test,y_test):
     plt.xlabel('fpr')
     plt.show()
     return accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test,predict_proba
-#######svm#########
-def svm_pre(x_train,y_train,x_test,y_test):
-    clf=svm.SVC(kernel='linear',probability=True)
+######logistic#######
+def log_pre(x_train,y_train,x_test,y_test):
+    clf=LogisticRegression(solver='liblinear',max_iter=40)
     clf.fit(x_train,y_train)
     accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test=classfision(clf,x_train,y_train,x_test,y_test)
     predict_proba=clf.predict_proba(x_test)
     print(predict_proba)
-    #predict_proba = clf.decision_function(y_test)
+ 
     fpr, tpr, thresholds = roc_curve(y_test, predict_proba[:, 1])
     roc_auc=auc(fpr,tpr)
-    print("SVM AUC:",roc_auc)
+    print("LOG AUC:",roc_auc)
     plt.figure()
-    plt.title('')
+    plt.title('ROC of LR-LMNN in test')
     plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
     plt.legend(loc='lower right')
     plt.plot([0,1],[0,1],'r--')
@@ -113,9 +151,32 @@ def svm_pre(x_train,y_train,x_test,y_test):
     plt.ylabel('tpr')
     plt.xlabel('fpr')
     plt.show()
+    plt.savefig("output_log.png", dpi=300)
+    return accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test,predict_proba
+#######svm#########
+def svm_pre(x_train,y_train,x_test,y_test):
+    clf=svm.SVC(kernel='linear',probability=True)
+    clf.fit(x_train,y_train)
+    accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test=classfision(clf,x_train,y_train,x_test,y_test)
+    predict_proba=clf.predict_proba(x_test)
+    print(predict_proba)
+    fpr, tpr, thresholds = roc_curve(y_test, predict_proba[:, 1])
+    roc_auc=auc(fpr,tpr)
+    print("SVM AUC:",roc_auc)
+    plt.figure()
+    plt.title('ROC of SVM-LMNN in test')
+    plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0,1],[0,1],'r--')
+    plt.xlim([0.0,1.0])
+    plt.ylim([0.0,1.0])
+    plt.ylabel('tpr')
+    plt.xlabel('fpr')
+    plt.show()
+    plt.savefig("output_svm.png", dpi=500)
     return accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test
 
-####预测结果
+###result
 def classfision(clf,x_train,y_train,x_test,y_test):
     predict_train=clf.predict(x_train)
     predict_test=clf.predict(x_test)
@@ -131,22 +192,23 @@ def classfision(clf,x_train,y_train,x_test,y_test):
     return accuracy_train,accuracy_test,recall_train,recall_test,precision_train,precision_test,f1_train,f1_test,predict_test
 
 if __name__ == '__main__':
-  #将excel表格的内容导入到列表中
-  ExcelFile=xlrd.open_workbook(r'E:\博士资料\2023.1-3杂物\度量学习论文\banyueban.xls')
+  #export excel
+  ExcelFile=xlrd.open_workbook(r'E:\banyueban.xls')
   table = ExcelFile.sheets()[0]
   tables =np.zeros(shape=(157,30))
   label=np.zeros(shape=(157,1))
   tables,label=import_excel(table)
   tables=data_preprocessing(tables)
-  #tables,score,P=Select(tables,label)
-  #tables,feature=Select(tables,label)
-  tables=metric_learn(tables,label)
   x_train,y_train,x_test,y_test=read_data(tables,label)
+  MLP_accuracy_train,MLP_accuracy_test,MLP_recall_train,MLP_recall_test,MLP_precision_train,MLP_precision_test,MLP_f1_train,MLP_f1_test,predict_test, predict_proba=MLP_pre(x_train,y_train,x_test,y_test)
+  boost_accuracy_train,boost_accuracy_test,boost_recall_train,boost_recall_test,boost_precision_train,boost_precision_test,boost_f1_train,boost_f1_test,predict_test, predict_proba=boost_pre(x_train,y_train,x_test,y_test)
+  RFF_accuracy_train,RFF_accuracy_test,RFF_recall_train,RFF_recall_test,RFF_precision_train,RFF_precision_test,RFF_f1_train,RFF_f1_test,predict_test, predict_proba=RFF_pre(x_train,y_train,x_test,y_test)
   svm_accuracy_train,svm_accuracy_test,svm_recall_train,svm_recall_test,svm_precision_train,svm_precision_test,svm_f1_train,svm_f1_test=svm_pre(x_train,y_train,x_test,y_test)
   log_accuracy_train,log_accuracy_test,log_recall_train,log_recall_test,log_precision_train,log_precision_test,log_f1_train,log_f1_test,predict_test, predict_proba=log_pre(x_train,y_train,x_test,y_test)
-  print("svm训练集准确度,召回率,精确度,F1:",svm_accuracy_train,svm_recall_train,svm_precision_train,svm_f1_train)
-  print("svm测试集准确度和召回率,精确度,F1:",svm_accuracy_test,svm_recall_test,svm_precision_test,svm_f1_test)
-  print("log训练集准确度和召回率,精确度,F1:",log_accuracy_train,log_recall_train,log_precision_train,log_f1_train)
-  print("log测试集准确度和召回率,精确度,F1:",log_accuracy_test,log_recall_test,log_precision_test,log_f1_test)
+  print("svm Accuracy,Recall,Precision,F1:",svm_accuracy_test,svm_recall_test,svm_precision_test,svm_f1_test)
+  print("log Accuracy,Recall,Precision,F1:",log_accuracy_test,log_recall_test,log_precision_test,log_f1_test)
+  print("boost Accuracy,Recall,Precision,F1:",boost_accuracy_test,boost_recall_test,boost_precision_test,boost_f1_test)
+  print("RFF Accuracy,Recall,Precision,F1:",RFF_accuracy_test,RFF_recall_test,RFF_precision_test,RFF_f1_test)
+  print("MLP Accuracy,Recall,Precision,F1:",MLP_accuracy_test,MLP_recall_test,MLP_precision_test,MLP_f1_test)
 
   
